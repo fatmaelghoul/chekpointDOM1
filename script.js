5225df28cc238042d3555adb6b869e8b7666c282
fatmaelghoul <fatouma_ghoul@live.com>
2024-11-25 13:38:20 +0100

    chekpoint color

---
+++ script.js
@@ -0,0 +1,9 @@
+const eltInput = document.getElementById("color");
+
+const saveBtn = document.getElementById("save");
+console.log(saveBtn);
+saveBtn.addEventListener("click", function () {
+    //console.log("ok")
+    body.style.backgroundColor=color.value
+
+})
